e7a78f91a5c08230d150770f77a3b0ae8ccdcb28
Kirill Sizov <kirill.sizov@cvat.ai>
2023-04-07 12:20:05 +0300

    Refactor webhooks (#5916)
    This PR fixes the following problems:
    - #5631
    - This will allow webhooks to be sent not only when an explicit request
    has been sent to the server.
    
    Co-authored-by: Nikita Manovich <nikita@cvat.ai>
    Co-authored-by: yasakova-anastasia <anastasia@cvat.ai>
    Co-authored-by: Roman Donchenko <roman@cvat.ai>

--- cvat/apps/webhooks/views.py
+++ cvat/apps/webhooks/views.py
@@ -71,11 +71,17 @@ class WebhookViewSet(viewsets.ModelViewSet):
     iam_organization_field = "organization"
 
     def get_serializer_class(self):
+<<<<<<< HEAD
         # Early exit for drf-spectacular compatibility
         if getattr(self, 'swagger_fake_view', False):
             return WebhookReadSerializer
 
         if self.request.path.endswith("redelivery") or self.request.path.endswith("ping"):
+=======
+        if self.request.path.endswith("redelivery") or self.request.path.endswith(
+            "ping"
+        ):
+>>>>>>> 7cff867ab (Refactor webhooks (#5916))
             return None
         else:
             if self.request.method in SAFE_METHODS:
@@ -176,9 +182,7 @@ class WebhookViewSet(viewsets.ModelViewSet):
     def redelivery(self, request, pk, delivery_id):
         delivery = WebhookDelivery.objects.get(webhook_id=pk, id=delivery_id)
         signal_redelivery.send(sender=self, data=delivery.request)
-
-        # Questionable: should we provide a body for this response?
-        return Response({})
+        return Response({}, status=status.HTTP_200_OK)
 
     @extend_schema(
         summary="Method send ping webhook",
